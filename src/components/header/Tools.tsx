9e8082f106e63109ca6042858f0e361785c54d6a
Lucas <lucas.lfs.souza@gmail.com>
2025-06-18 10:36:40 +0200

    changed tools text colors

--- src/components/header/Tools.tsx
+++ src/components/header/Tools.tsx
@@ -4,55 +4,55 @@ const Tools = () => {
       <div className='border-b border-white pb-8'>
         <div className='text-white'>Tools</div>
         <div>
-          <div className='bg-white/5 rounded-4xl p-2 inline-block text-xs my-2 mr-2'>
+          <div className='bg-white/5 rounded-4xl p-2 inline-block text-xs my-2 mr-2 text-white'>
             JavaScript
           </div>
-          <div className='bg-white/5 rounded-4xl p-2 inline-block text-xs my-2 mr-2'>
+          <div className='bg-white/5 rounded-4xl p-2 inline-block text-xs my-2 mr-2 text-white'>
             React
           </div>
-          <div className='bg-white/5 rounded-4xl p-2 inline-block text-xs my-2 mr-2'>
+          <div className='bg-white/5 rounded-4xl p-2 inline-block text-xs my-2 mr-2 text-white'>
             Typescript
           </div>
-          <div className='bg-white/5 rounded-4xl p-2 inline-block text-xs my-2 mr-2'>
+          <div className='bg-white/5 rounded-4xl p-2 inline-block text-xs my-2 mr-2 text-white'>
             NextJS
           </div>
-          <div className='bg-white/5 rounded-4xl p-2 inline-block text-xs my-2 mr-2'>
+          <div className='bg-white/5 rounded-4xl p-2 inline-block text-xs my-2 mr-2 text-white'>
             Tailwind
           </div>
-          <div className='bg-white/5 rounded-4xl p-2 inline-block text-xs my-2 mr-2'>
+          <div className='bg-white/5 rounded-4xl p-2 inline-block text-xs my-2 mr-2 text-white'>
             CSS
           </div>
-          <div className='bg-white/5 rounded-4xl p-2 inline-block text-xs my-2 mr-2'>
+          <div className='bg-white/5 rounded-4xl p-2 inline-block text-xs my-2 mr-2 text-white'>
             HTML
           </div>
-          <div className='bg-white/5 rounded-4xl p-2 inline-block text-xs my-2 mr-2'>
+          <div className='bg-white/5 rounded-4xl p-2 inline-block text-xs my-2 mr-2 text-white'>
             Redux
           </div>
-          <div className='bg-white/5 rounded-4xl p-2 inline-block text-xs my-2 mr-2'>
+          <div className='bg-white/5 rounded-4xl p-2 inline-block text-xs my-2 mr-2 text-white'>
             SCSS
           </div>
-          <div className='bg-white/5 rounded-4xl p-2 inline-block text-xs my-2 mr-2'>
+          <div className='bg-white/5 rounded-4xl p-2 inline-block text-xs my-2 mr-2 text-white'>
             Axios
           </div>
-          <div className='bg-white/5 rounded-4xl p-2 inline-block text-xs my-2 mr-2'>
+          <div className='bg-white/5 rounded-4xl p-2 inline-block text-xs my-2 mr-2 text-white'>
             Material UI
           </div>
-          <div className='bg-white/5 rounded-4xl p-2 inline-block text-xs my-2 mr-2'>
+          <div className='bg-white/5 rounded-4xl p-2 inline-block text-xs my-2 mr-2 text-white'>
             Git
           </div>
-          <div className='bg-white/5 rounded-4xl p-2 inline-block text-xs my-2 mr-2'>
+          <div className='bg-white/5 rounded-4xl p-2 inline-block text-xs my-2 mr-2 text-white'>
             GitHub
           </div>
-          <div className='bg-white/5 rounded-4xl p-2 inline-block text-xs my-2 mr-2'>
+          <div className='bg-white/5 rounded-4xl p-2 inline-block text-xs my-2 mr-2 text-white'>
             Azure
           </div>
-          <div className='bg-white/5 rounded-4xl p-2 inline-block text-xs my-2 mr-2'>
+          <div className='bg-white/5 rounded-4xl p-2 inline-block text-xs my-2 mr-2 text-white'>
             BitBucket
           </div>
-          <div className='bg-white/5 rounded-4xl p-2 inline-block text-xs my-2 mr-2'>
+          <div className='bg-white/5 rounded-4xl p-2 inline-block text-xs my-2 mr-2 text-white'>
             Jira
           </div>
-          <div className='bg-white/5 rounded-4xl p-2 inline-block text-xs my-2 mr-2'>
+          <div className='bg-white/5 rounded-4xl p-2 inline-block text-xs my-2 mr-2 text-white'>
             Figma
           </div>
         </div>
