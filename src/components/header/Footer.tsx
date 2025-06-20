8ef3c11e3d33dc6ac6000cbfa4ffc2797a290449
Lucas <lucas.lfs.souza@gmail.com>
2025-06-20 15:29:21 +0200

    added border to header footer

--- src/components/header/Footer.tsx
+++ src/components/header/Footer.tsx
@@ -2,7 +2,7 @@ import { GitHub, LinkedIn } from '@mui/icons-material';
 
 const Footer = () => {
   return (
-    <div className='text-xl sm:text-3xl flex gap-x-8 sm:gap-x-16 items center justify-center text-white bg-white/10 rounded-b-xl bottom-0 p-4 items-center'>
+    <div className='text-xl sm:text-3xl flex gap-x-8 sm:gap-x-16 items center justify-center text-white bg-white/10 rounded-b-xl bottom-0 p-4 items-center border border-black'>
       <a
         className='hover:scale-125 ease-in-out duration-700'
         href='https://github.com/souzalucasdev'
